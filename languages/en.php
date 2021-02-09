014b1db35bb1281bcc4332c484491b64a0ec22e8
Jerome Bakker <jeabakker@coldtrick.com>
2021-02-09 13:43:32 +0100

    chore: translations

---
+++ languages/en.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'pwa:settings:display_mode' => "Display mode",
+	'pwa:settings:display_mode:help' => "Which User Interface (UI) should be used for the webapp",
+	'pwa:settings:display_mode:fullscreen' => "Fullscreen",
+	'pwa:settings:display_mode:standalone' => "Standalone",
+	'pwa:settings:display_mode:minimal-ui' => "Minumal UI",
+	'pwa:settings:display_mode:browser' => "Browser",
+	'pwa:settings:background_color' => "Splash screen background color",
+	'pwa:settings:background_color:help' => "Which background color should be used on the splash screen",
+	'pwa:settings:theme_color' => "Theme color",
+	'pwa:settings:theme_color:help' => "Set the color of the tool bar",
+	'pwa:settings:icons' => "Webapp icon used on the splash screen",
+];
---
+++ languages/nl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file was created by Translation Editor v8.0
+ * On 2021-02-09 13:43
+ */
+
+return array (
+  'pwa:settings:display_mode' => 'Weergave modus',
+  'pwa:settings:display_mode:help' => 'Welk User Interface (UI) moet er worden gebruikt voor de webapp',
+  'pwa:settings:display_mode:fullscreen' => 'Volledig scherm',
+  'pwa:settings:display_mode:standalone' => 'Alleenstaand',
+  'pwa:settings:display_mode:minimal-ui' => 'Minimale UI',
+  'pwa:settings:display_mode:browser' => 'Browser',
+  'pwa:settings:background_color' => 'Opstartscherm achtergrond kleur',
+  'pwa:settings:background_color:help' => 'Welke achtergrond kleur moet er worden gebruik op het opstartscherm',
+  'pwa:settings:theme_color' => 'Theme kleur',
+  'pwa:settings:theme_color:help' => 'Stel de kleur van de taakbalk in',
+  'pwa:settings:icons' => 'Webapp icoon welke gebruikt wordt op het opstartscherm',
+);
