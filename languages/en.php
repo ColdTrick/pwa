dc3b42421153569024690d3e3599a1a10288d6eb
Jeroen Dalsem <jdalsem@coldtrick.com>
2024-11-25 10:46:55 +0100

    chore: fixed a typo

--- languages/en.php
+++ languages/en.php
@@ -5,7 +5,7 @@ return [
 	'pwa:settings:display_mode:help' => "Which User Interface (UI) should be used for the webapp",
 	'pwa:settings:display_mode:fullscreen' => "Fullscreen",
 	'pwa:settings:display_mode:standalone' => "Standalone",
-	'pwa:settings:display_mode:minimal-ui' => "Minumal UI",
+	'pwa:settings:display_mode:minimal-ui' => "Minimal UI",
 	'pwa:settings:display_mode:browser' => "Browser",
 	'pwa:settings:background_color' => "Splash screen background color",
 	'pwa:settings:background_color:help' => "Which background color should be used on the splash screen",
