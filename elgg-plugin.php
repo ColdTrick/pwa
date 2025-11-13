da7a96f657c4b95a0b9a528e0b111d7619cb4588
Jerôme Bakker <jeabakker@coldtrick.com>
2025-11-13 13:35:41 +0100

    chore: wrapping up v5.0.2

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -9,7 +9,7 @@ if (is_dir(__DIR__ . '/vendor')) {
 
 return [
 	'plugin' => [
-		'version' => '5.0.1',
+		'version' => '5.0.2',
 		'name' => 'Progressive Web App',
 	],
 	'bootstrap' => Bootstrap::class,
